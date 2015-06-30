c08e3227c15e8d3940d8150d741ef988b8bc4d82
Audrey Carlsen <acarlsen@seattletimes.com>
2015-06-30 15:02:14 -0700

    fun times with pinterest images

--- src/js/main.js
+++ src/js/main.js
@@ -117,7 +117,7 @@ var calculateResult = function() {
       }
       new Share(".share-results", {
         description: "I scored " + result.score + "/12! Think you can pronounce the names of these Washington places?",
-        image: "./assets/results/" + result.image,
+        image: "http://apps-stage.seattletimesdata.com/extensive-voodoo/assets/fb_pysht.JPG",
         ui: {
           flyout: "bottom right"
         },
